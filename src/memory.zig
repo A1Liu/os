282eb48de168f1ae1560b4771f091db3861ef5c5
Albert Liu <albertymliu@gmail.com>
2022-08-22 06:10:19 -0400

    meh

--- src/memory.zig
+++ src/memory.zig
@@ -7,6 +7,8 @@ pub const size = 1024 * 1024 * 1024;
 // 3. Write asm for setting mapping and then jumping to correct location
 // 4. Profit?
 
+// NOTE: I can't use the eventual address of the memory map in the
+// computation, so some of the data needs to be written at runtime.
 export var kernel_memory_map align(4096) = map: {
     var map = [1]u64{0} ** (4096 / 8 * 3);
     break :map map;
