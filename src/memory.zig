cf43c588359a8eaa1e4cdd97030bd4d057227180
Albert Liu <albertymliu@gmail.com>
2022-08-22 06:08:36 -0400

    meh

--- src/memory.zig
+++ src/memory.zig
@@ -7,7 +7,7 @@ pub const size = 1024 * 1024 * 1024;
 // 3. Write asm for setting mapping and then jumping to correct location
 // 4. Profit?
 
-export var kernel_memory_map = map: {
-    var map = [1]u8{0} ** (4096 * 3);
+export var kernel_memory_map align(4096) = map: {
+    var map = [1]u64{0} ** (4096 / 8 * 3);
     break :map map;
 };
